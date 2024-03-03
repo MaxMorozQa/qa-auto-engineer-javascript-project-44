dc3e0eb6aa48338d3a4ff495ca249337acad9204
Max Pyhov <maksim-spartac@mail.ru>
2024-03-03 12:20:00 +0300

    add new line

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+const Greeting = () => {
+
+var readlineSync = require('readline-sync');
+
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+
+} 
+
+export default Greeting;
